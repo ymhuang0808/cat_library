ee447fe920575e512a7e9e484805c0bdc573550a
YMHuang <ym.huang0808@gmail.com>
2015-08-15 12:28:17 +0800

    Add a new type column into books table

--- books/app/models/book.rb
+++ books/app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ActiveRecord::Base
+  enum type: { existing: 0, wish_list:1 }
 end
--- books/db/schema.rb
+++ books/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708085248) do
+ActiveRecord::Schema.define(version: 20150815042519) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150708085248) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.text     "notes"
+    t.integer  "type"
   end
 
 end
